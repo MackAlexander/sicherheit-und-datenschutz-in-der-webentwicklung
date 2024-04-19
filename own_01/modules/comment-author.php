2f0c05a14c55860a2696c8e5bd5462741f86f70e
nickeljonas <diedingen@web.de>
2024-04-19 12:12:54 +0200

    simplified comment author

--- own_01/modules/comment-author.php
+++ own_01/modules/comment-author.php
@@ -14,10 +14,7 @@ class Comment
      */
     public static function comment_author_display_name($comment_id)
     {
-        $comment = get_comment($comment_id);
-        $display_name = $comment->comment_author;
-        $display_name = 'Fake Comment Autor';
-        return $display_name;
+        return 'Fake Comment Autor';
     }
 
     /**
@@ -25,10 +22,7 @@ class Comment
      */
     public static function comment_author_url($comment_id)
     {
-        $comment = get_comment($comment_id);
-        $author_url = $comment->comment_author_url;
-        $author_url = 'localhost/author/fakeauthor';
-        return $author_url;
+        return 'localhost/author/fakeauthor';
     }
 }
 
