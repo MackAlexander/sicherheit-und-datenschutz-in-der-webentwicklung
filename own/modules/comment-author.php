bbe3660fb97ac978553076f6a75843d3bb692a5e
nickeljonas <diedingen@web.de>
2024-04-20 11:11:00 +0200

    update comment author name and url

--- own/modules/comment-author.php
+++ own/modules/comment-author.php
@@ -4,25 +4,27 @@ namespace THM\Own;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-add_filter('get_comment_author', ['THM\Own\Comment', 'comment_author_display_name'], 10, 1);
-add_filter('get_comment_author_url', ['THM\Own\Comment', 'comment_author_url'], 10, 1);
+add_filter('get_comment_author', ['THM\Own\Comment', 'comment_author_display_name'], 10, 3);
+add_filter('get_comment_author_url', ['THM\Own\Comment', 'comment_author_url'], 10, 3);
 
 class Comment
 {
     /**
      * Change the display name of the comment author
      */
-    public static function comment_author_display_name($comment_id)
+    public static function comment_author_display_name($comment_author, $comment_id, $comment)
     {
-        return 'Fake Comment Autor';
+        $comment_author = 'Fake Comment Autor';
+        return $comment_author;
     }
 
     /**
      * Change the url of the comment author
      */
-    public static function comment_author_url($comment_id)
+    public static function comment_author_url($comment_author_url, $comment_id, $comment)
     {
-        return 'localhost/author/fakeauthor';
+        $comment_author_url = 'localhost/author/fakecommentauthor';
+        return $comment_author_url;
     }
 }
 
