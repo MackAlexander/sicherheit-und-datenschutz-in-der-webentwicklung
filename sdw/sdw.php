9f9ca5388a6b81dd23306068f19b1bbe3e9004bb
nickeljonas <diedingen@web.de>
2024-05-24 16:51:30 +0200

    initial commit

--- sdw/sdw.php
+++ sdw/sdw.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: SDW-04
-Description: Sicherheit und Datenschutz in der Webentwicklung - Kapitel 4
+Plugin Name: SDW
+Description: Sicherheit und Datenschutz in der Webentwicklung - Kapitel 5
 Version: 1.0.0
 Author: Technische Hochschule Mittelhessen
 Author URI: https://www.thm.de
@@ -11,6 +11,6 @@ namespace THM\Security;
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-require_once(dirname(__FILE__) . '/modules/username-enumeration.php');
+require_once(dirname(__FILE__) . '/modules/classifier.php');
 
 ?>
--- sdw_04/RESEARCH.md
+++
@@ -1,3 +0,0 @@
-# Daten sammeln
-
-## Welche Daten sammlen wir?
